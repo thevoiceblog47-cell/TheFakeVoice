f546deec23f3087d344b8e517eb222730dc7b690
thevoiceblog47-cell <thevoiceblog47@gmail.com>
2026-09-20 17:51:34 -0400

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.VOICE_CONFIG = {
-  supabaseUrl: 'https://xudayjamephscnvketgb.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1ZGF5amFtZXBoc2NudmtldGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4Nzg5NTgsImV4cCI6MjEwNTQ1NDk1OH0.1RbU5RqL_pnd8iNaQa-pfUCji-_sT0a6qVj1cf5dPyM'
-};
